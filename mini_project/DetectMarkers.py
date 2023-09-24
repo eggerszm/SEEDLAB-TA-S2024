c7bcfbecab166e5293c949b29a0650a0f6cb40fe
Elijah Price <elijahprice@mines.edu>
2023-09-24 12:01:46 -0900

    Marker detection and LCD info

--- mini_project/DetectMarkers.py
+++ mini_project/DetectMarkers.py
@@ -5,26 +5,38 @@ SEED Lab Group 9, Fall 2023
 """
 
 import cv2
+import enum
 import time
 import numpy as np
 from cv2 import aruco
+from smbus2 import SMBus
+from typing import Tuple
 
-from Exercise1b import init_lcd
+import LCDInit
+
+IMG_X_WIDTH = 640
+IMG_Y_HEIGHT = 480
+
+ARD_ADDR = 8
+
+class Quadrant(enum.Enum):
+    NW = 0
+    NE = 1
+    SW = 2
+    SE = 3
 
 def main():
-    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
     
     camera = cv2.VideoCapture(0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_X_WIDTH)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_Y_HEIGHT)
     camera.set(cv2.CAP_PROP_BRIGHTNESS, 100)
 
     time.sleep(0.5)
 
-    lcd = init_lcd()
-    lcd.color = [100, 100, 100]
-
-    current_msg = "No markers found"
+    piLCD = LCDInit.LCD()
+    ard = SMBus(1)
 
     while True:
         _, img = camera.read()
@@ -35,23 +47,60 @@ def main():
 
         overlay = aruco.drawDetectedMarkers(img, corners, borderColor=4)
 
-        if ids is not None:
-            msg = ""
-            for id in ids:
-                msg += f"{str(id[0])} "
+        if len(corners) == 1:
+            ctr = get_center(corners[0])
+            overlay = cv2.circle(overlay, ctr, radius=2, color=(0,0,255), thickness=-1)
+            current_quadrant = get_point_quadrant(ctr)
+            piLCD.write_lcd(current_quadrant.name)
+        elif corners == ():
+            current_quadrant = None
+            piLCD.write_lcd("No markers")
         else:
-            msg = "No markers found"
-                
-        if msg != current_msg:
-            current_msg = msg
-            lcd.clear()
-            lcd.message = current_msg
+            current_quadrant = None
+            for c in corners:
+                overlay = cv2.circle(overlay, get_center(c), radius=2, color=(0,0,255), thickness=-1)
+            piLCD.write_lcd(f"ERROR:\n{len(corners)} markers found")
+         
+        # Draw quadrants
+        overlay = cv2.line(overlay, (0, int(IMG_Y_HEIGHT/2)), (IMG_X_WIDTH, int(IMG_Y_HEIGHT/2)), (0,0,255), 1)
+        overlay = cv2.line(overlay, (int(IMG_X_WIDTH/2), 0), (int(IMG_X_WIDTH/2), IMG_Y_HEIGHT), (0,0,255), 1)
 
         cv2.imshow("overlay", overlay)
+
         if (cv2.waitKey(1) & 0xFF) == ord("q"):
             cv2.destroyAllWindows()
-            lcd.color = [0, 0, 0]
+            piLCD.cleanup()
+            ard.close()
             break
+
+def get_center(corners: np.array) -> Tuple[int, int]:
+    """
+    Take a corners array and return its center point
+    Args:
+        corners (np.array): shape (1,4,2), contains the four corners of the marker
+    Returns:
+        Tuple[int, int]: coordinates of the marker in the frame
+    """
+    xctr = (corners[0,0,0] + corners[0,1,0] + corners[0,2,0] + corners[0,3,0])/4
+    yctr = (corners[0,0,1] + corners[0,1,1] + corners[0,2,1] + corners[0,3,1])/4
+    return (int(xctr), int(yctr))
+
+def get_point_quadrant(point: Tuple[int, int]) -> Quadrant:
+    """
+    Get the quadrant a point falls into, where (0,0) is the NW corner
+    Args:
+        point (Tuple[int, int]): (X, Y) coordinate pair to classify
+    Returns:
+        Quadrant: Quadrant of point, from enum
+    """
+    if point[0] <= IMG_X_WIDTH/2 and point[1] <= IMG_Y_HEIGHT/2:
+        return Quadrant.NW
+    elif point[0] <= IMG_Y_HEIGHT/2:
+        return Quadrant.SW
+    elif point[1] <= IMG_X_WIDTH/2:
+        return Quadrant.NE
+    else:
+        return Quadrant.SE
 
 
 if __name__ == "__main__":
--- mini_project/LCDInit.py
+++ mini_project/LCDInit.py
@@ -1,9 +1,5 @@
 """
-Exercise 1a
-
-Elijah Price
-
-Get user input [0, 100], send it to the arduino, request input + 100, and display to LCD
+LCD handler functions, including current state tracking
 """
 
 import board
@@ -11,19 +7,28 @@ import time
 from smbus2 import SMBus
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
-ARD_ADDR = 8
+class LCD():
+
+    def __init__(self):
+        """Initialize the LCD and prepare it to display text
+
+        Returns:
+            character_lcd.Character_LCD_RGB_I2C: LCD connected
+        """
+        self.lcd = character_lcd.Character_LCD_RGB_I2C(board.I2C(), 16, 2)
+        self.lcd.clear()
+        self.lcd.text_direction = self.lcd.LEFT_TO_RIGHT
+        self.current_msg = ""
+        self.lcd.color = [100, 100, 100]
+
+    def cleanup(self):
+        self.lcd.color = [0,0,0]
+        self.lcd.clear()
 
 
-def init_lcd() -> character_lcd.Character_LCD_RGB_I2C:
-    """Initialize the LCD and prepare it to display text
-
-    Returns:
-        character_lcd.Character_LCD_RGB_I2C: LCD connected
-    """
-    lcd_columns, lcd_rows = 16, 2
-    lcd = character_lcd.Character_LCD_RGB_I2C(board.I2C(), lcd_columns, lcd_rows)
-    lcd.clear()
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    return lcd
-
+    def write_lcd(self, msg: str):
+        if msg != self.current_msg:
+            self.lcd.clear()
+            self.lcd.message = msg
+            self.current_msg = msg
 
