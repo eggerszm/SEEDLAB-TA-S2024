dd596c52352909dfe8472a1f9866e5f2176fe772
Zander E <145614296+eggerszm@users.noreply.github.com>
2023-10-12 20:16:35 -0600

    Implemented experimental angular control
    Used PD. Need to tune constants

--- demo1/rotationalControl/rotationalControl.ino
+++ demo1/rotationalControl/rotationalControl.ino
@@ -14,6 +14,9 @@ Goal: Unsure ATM
 #define KpRIGHT 0.012
 #define KiRIGHT 0.00019
 
+#define KpANGLE 0.012 // Completely arbitrary. I don't want to do simulation and don't have access to the robot
+#define KdANGLE 0.0002 // See above
+
 #define TARGET_FEET 10.0
 #define TARGET_ANGLE_RADIANS 0
 #define MAX_PWM 100
@@ -24,10 +27,13 @@ double voltDelta; // {Delta}V_a as listed in SteeringControl_handout.pdf -> V_ri
 double voltageLeft, voltageRight;
 double integralLeft, integralRight;
 
+double derivativeAngle = 0.0;
+double prevErrorAngle = 0;
+
 float currentTime; // Current time in seconds
 unsigned long lastTimeMs = 0; // Time at which last loop finished running in ms
 unsigned long startTimeMs; // Time when the code starts running in ms
-unsigned long desiredTsMs = 5; // Desired sampling time in ms
+unsigned long desiredTsMs = 5; // Desired sampling time in ms -- previously was 10, may want to change back
 
 
 
@@ -58,11 +64,12 @@ void setup() {
 void loop() {
   currentTime = (float)(lastTimeMs - startTimeMs)/1000;
 
-  long currentPosLeft = -EncLeft.read();
+  long currentPosLeft = -EncLeft.read(); //Negative because this motor is facing "backwards"
   long currentPosRight = EncRight.read();
 
 
   // Linear Distance Control
+  // PI Control
   long targetPos = 2070 * TARGET_FEET; // 3200.0 / (15.0 * PI) * 30.48
 
   long errorLeft = targetPos - currentPosLeft;
@@ -77,15 +84,27 @@ void loop() {
 
   // Angle Control
   long targetAngle = TARGET_ANGLE_RADIANS;
-  double thetaRight = 2.0 * PI * (double)((double)currentPosRight / 3200.0);
-  double thetaLeft = 2.0 * PI * (double)((double)currentPosLeft / 3200.0);
+  double thetaRight = 2.0 * PI * (double)((double)currentPosRight / 3200.0); // Right Wheel Position in Radians
+  double thetaLeft = 2.0 * PI * (double)((double)currentPosLeft / 3200.0); // Left Wheel Position in Radians
   long currentAngle = WHEEL_DIAMETER_IN_CM * (thetaRight - thetaLeft) / ROBOT_DIAMETER_IN_CM;
 
-  // TO DO: Implement control system for this. Probably PD?
+  // This is a basic PD control.
+  // TO DO: tune PD, decide if I is necessary 
+  //      : test this, could be completely incorrect
+  double errorAngle = targetAngle - currentAngle;
 
+  if (prevErrorAngle == 0) { // Don't do on the first loop
+    derivativeAngle = 0;
+  } else {
+    derivativeAngle = (errorAngle - prevErrorAngle) / desiredTsMs;
+  }
 
-  voltageRight = (linearVoltRight + voltDelta) / 2;
-  voltageLeft = (linearVoltLeft - voltDelta) / 2;
+  prevErrorAngle = errorAngle;
+  voltDelta = KpANGLE * errorAngle + KdANGLE * derivativeAngle;
+
+  // Set voltages
+  voltageRight = (linearVoltRight + voltDelta);
+  voltageLeft = (linearVoltLeft - voltDelta);
 
   // H-Bridge Direction
   if(voltageLeft > 0) {
@@ -101,10 +120,10 @@ void loop() {
   }
 
   // Powering the motors
-  int PWMLeft = 255 * abs(voltageLeft)/BATTERY_VOLTAGE;
-  analogWrite(9, min(PWMLeft, MAX_PWM) );
+  int PWMLeft = 255 * abs(voltageLeft) / BATTERY_VOLTAGE;
+  analogWrite(9, min(PWMLeft, MAX_PWM));
 
-  int PWMRight = 255 * abs(voltageRight)/BATTERY_VOLTAGE;
+  int PWMRight = 255 * abs(voltageRight) / BATTERY_VOLTAGE;
   analogWrite(10, min(PWMRight, MAX_PWM));
 
 
